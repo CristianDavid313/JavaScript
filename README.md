ce5efbb7571f80cc311ac6193fb8fce964a7780b
Cristian <cristianvargasavellaneda@gmail.com>
2024-10-13 14:51:50 -0500

    Creación de la teoria basica y la teoria DOM

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-Configuración inicial de README.md
+# Guía JavaScript
+
+Este es un repositorio simple sobre JavaScript y como aplicarlo así a el desarrollo web.
+
+## Comenzando 🚀
+
+Para clonar este repositorio dirígete a la carpeta en la que quieres guardarlo y ejecuta el siguiente comando en la terminal
+
+```
+git clone https://github.com/CristianDavid313/JavaScript.git
+```
+
+## Índice 📢
+
+* En la carpeta **TeoriaBasica** encontrará los conceptos básicos de JavaScript como lo son los tipos de variables, condicionales, bucles, etc.
+
+* En la carpeta **TeoriaDOM** encontrará los conceptos básicos de JavaScript y como interactuar con HTML.
+
+![Nombre del proyecto](https://github.com/CristianDavid313/JavaScript/blob/main/Capturas/01_MenuBasico.png)
+
+## Referencias 🔧
+
+Para profundizar en los temas puedes revisar:
+
+* [Nombre de la pagina](Link) - Ejemplo
+
+---
+😁
---
+++ TeoriaBasica/index.js
@@ -0,0 +1,64 @@
+console.log("Hola mundo");
+
+//Variables
+var variable="hola";
+console.log(variable);
+
+//Constantes
+const constante=5;
+console.log(constante);
+
+//Las variables pueden cambiar de valor a lo largo del codigo pero las constantes no.
+
+//Operaciones con variables
+var numero1=4;
+var numero2=4;
+var solucion=Math.pow(numero1,numero2);
+console.log("La potencia es: " + solucion);
+
+//Operadore relacionales
+var resultado=numero1==numero2;
+console.log("El resultado es: " + resultado);
+
+//Operadore logicos
+var resultado=(numero1==numero2) && (numero1!=numero2);
+console.log("El resultado es: " + resultado);
+
+//Operadores de asignación
+var a=2;
+a+=5;
+console.log("El resultado es: " + a)
+
+//Condicionales
+var numero=7;
+if(numero==7){
+    console.log("El número es igual a 7");
+}
+else if(numero==8){
+    console.log("El número es igual a 8");
+}
+else{
+    console.log("El numero es diferente");
+}
+
+//Bucle while
+var i=0;
+while(i<10){
+    i+=1;
+    console.log(i)
+}
+
+//Ciclo for
+for(var i=0; i<=10; i++){
+    console.log(i);
+}
+
+//Funciones
+var funciones="Correcto";
+function cafetera(variable1,variable2){
+    if(variable1==1 && variable2==2){
+        return funciones;
+    }
+}
+var final=cafetera(1,2);
+console.log(final);
